1d9533e276db790581409044b6752ca822efac29
Navinda Lankesh <adilanga@gmail.com>
2022-03-09 06:55:15 +0530

    Lecture1

---
+++ ctse_lecture1/android/app/src/main/kotlin/com/sliit/ctse/ctse_lecture1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.sliit.ctse.ctse_lecture1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ ctse_lecture1/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text('Welcome to Flutter'),
+        ),
+        body: const Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
